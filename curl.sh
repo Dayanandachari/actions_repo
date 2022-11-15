ee767fc11d856bb0dbc2e80810ef258cb8e69a33
Dayanandachari <101535221+Dayanandachari@users.noreply.github.com>
2022-11-15 22:48:11 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,1 +1,8 @@
+!#/bin/bash
 
+curl   \
+       -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: token $GITHUB_TOKEN " \
+        https://api.github.com/repos/${{ inputs.Repo_owner }}/${{ inputs.Repo_name }}/issues/${{ inputs.PR_number}}/comments \
+        -d '{"body":"${{inputs.Message }}"}'
